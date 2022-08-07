c2671d701ed58335c8ec58495e29c949057dd0c8
DESKTOP-4928LN9\dudko <dudko.na@gmail.com>
2022-08-07 17:49:43 +0200

    corrections

--- tobe/src/main/java/com/vet/controllers/VetController.java
+++ tobe/src/main/java/com/vet/controllers/VetController.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 @RestController(value = "/v2")
--- tobe/src/main/java/com/vet/services/VettingAgency.java
+++ tobe/src/main/java/com/vet/services/VettingAgency.java
@@ -3,8 +3,6 @@ package com.vet.services;
 import com.vet.model.VettingResults;
 import reactor.core.publisher.Mono;
 
-import java.util.function.Function;
-
 public interface VettingAgency {
 
     Mono<VettingResults> vet(String name);
